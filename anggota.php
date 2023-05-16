d2ee7871af52f336421f946f631678045bd1eae6
septian693 <125529137+septian693@users.noreply.github.com>
2023-05-16 23:47:26 +0700

    Add files via upload

---
+++ anggota.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+if(!isset($_SESSION['session_username'])){
+    header("location:login.php");
+    exit();
+}
+print_r($_SESSION);
+print_r($_COOKIE);
+header('location: ../ToDoList/index.php');
